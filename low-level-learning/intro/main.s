6ab5e7ee00d7deff8fa851ce18f8174e7c0aa1e0
Victor Payno <vpayno@users.noreply.github.com>
2023-05-10 23:32:07 -0700

    intro: add exit system call

--- low-level-learning/intro/main.s
+++ low-level-learning/intro/main.s
@@ -2,3 +2,7 @@
 .intel_syntax noprefix
 
 _start:
+    // sys_exit
+    mov rax, 60                 # system call number for sys_exit
+    mov rdi, 0                  # exit code
+    syscall                     # invoke system call in rax
